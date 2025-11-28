45674b5e095b2735f5badd5a4fd6abc0aac62ade
Nika <nika.gossamer@gmail.com>
2025-11-29 01:19:25 +0200

    update prettier import sorting & add ecsstastic for evaluation

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import { ecsstatic } from "@acab/ecsstatic/vite";
 import tailwind from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid(), tailwind()],
+  plugins: [solid(), tailwind(), ecsstatic()],
   resolve: {
     alias: { "~": "/app" },
   },
